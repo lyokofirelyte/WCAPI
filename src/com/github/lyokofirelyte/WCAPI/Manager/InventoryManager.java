54a7b13845c8f4dd166f9e9941032cd88e44a2ad
Jesse Bryan <winneonsword@gmail.com>
2013-12-22 20:43:14 -0600

    Test

--- src/com/github/lyokofirelyte/WCAPI/Manager/InventoryManager.java
+++ src/com/github/lyokofirelyte/WCAPI/Manager/InventoryManager.java
@@ -1,6 +1,7 @@
 package com.github.lyokofirelyte.WCAPI.Manager;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Material;
@@ -9,6 +10,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import com.github.lyokofirelyte.WCAPI.WCAPI;
+import com.github.lyokofirelyte.WCAPI.WCManager;
 
 public class InventoryManager {
 	
@@ -39,4 +41,73 @@ public class InventoryManager {
 		
 		return i;
 	}
+	
+	public static ItemStack createItem(String display, String[] lore, Material mat, int... data){
+		
+		ItemStack item = null;
+		
+		switch (data.length){
+		
+		default:
+			
+			item = new ItemStack(mat, 1);
+			break;
+			
+		case 1:
+			
+			item = new ItemStack(mat, data[0]);
+			break;
+			
+		case 2:
+			
+			item = new ItemStack(mat, data[0], (short) data[1]);
+			break;
+			
+		}
+		
+		ItemMeta meta = item.getItemMeta();
+		lore = WCManager.AS(lore);
+		List<String> loreL = Arrays.asList(lore);
+		
+		meta.setDisplayName(WCManager.AS(display));
+		meta.setLore(loreL);
+		item.setItemMeta(meta);
+		
+		return item;
+		
+	}
+	
+	public static ItemStack createItem(String display, String[] lore, short durability, Material mat, int... data){
+		
+		ItemStack item = createItem(display, lore, mat, data);
+		item.setDurability(durability);
+		
+		return item;
+		
+	}
+	
+	public static ItemStack createItem(String display, String[] lore, Enchantment enchant, int amp, Material mat, int... data){
+		
+		ItemStack item = createItem(display, lore, mat, data);
+		ItemMeta meta = item.getItemMeta();
+		
+		meta.addEnchant(enchant, amp, true);
+		item.setItemMeta(meta);
+		
+		return item;
+		
+	}
+	
+	public static ItemStack createItem(String display, String[] lore, Enchantment enchant, int amp, short durability, Material mat, int... data){
+		
+		ItemStack item = createItem(display, lore, durability, mat, data);
+		ItemMeta meta = item.getItemMeta();
+		
+		meta.addEnchant(enchant, amp, true);
+		item.setItemMeta(meta);
+		
+		return item;
+		
+	}
+	
 }
