5f4336ba2c2e4b11889479f0fcb841fa60ab9295
lyokofirelyte <lyokofirelyte@gmail.com>
2014-01-10 02:28:12 -0600

    Maven

---
+++ src/main/java/com/github/lyokofirelyte/WCAPI/Manager/RebootManager.java
@@ -0,0 +1,64 @@
+package com.github.lyokofirelyte.WCAPI.Manager;
+
+import org.bukkit.Bukkit;
+
+import com.github.lyokofirelyte.WCAPI.WCAPI;
+import com.github.lyokofirelyte.WCAPI.WCLink;
+import com.github.lyokofirelyte.WCAPI.WCSystem;
+
+
+public class RebootManager extends WCLink {
+
+	public RebootManager(WCAPI i) {
+		super(i);
+	}
+
+	public String WC = "§dWC §5// §d";
+
+	
+	long delay = 0L;
+	
+	public void scheduleReboot(){
+		
+    	Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(pl, new Runnable(){
+        public void run(){
+        	  wcReboot();
+        }
+        }
+        , 1728000);	
+	}
+
+	public void wcReboot(){
+		
+		WCSystem wcs = pl.wcm.getWCSystem("system");
+		wcs.setRebooting(true);
+		pl.wcm.updateSystem("system", wcs);
+
+		Bukkit.broadcastMessage(WC + "The server will be executing the daily reboot in 5 minutes.");
+		
+		for (int x = 5; x > 0; x--){
+		
+			delay = delay + 1200L;
+			
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(pl, new Runnable(){
+		    public void run() {
+		    	Bukkit.broadcastMessage(WC + "The server will be executing the daily reboot in less than 5 minutes.");
+		    }
+		    }
+		    , delay);
+			
+			if (x == 1){
+				
+				delay = delay + 1200L;
+				
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(pl, new Runnable(){
+			    public void run(){
+			    	Bukkit.broadcastMessage(WC + "The server is rebooting. Don't worry, we've saved the world! See you in 60.");
+			    	Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "stop");
+			    }
+			    }
+			    , delay);
+			}
+		}
+	}
+}
